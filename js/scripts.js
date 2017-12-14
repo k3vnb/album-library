5303128b4488c0d26ffe5be92ab9639968cb69da
Kevin Boyle and Zachary Marston <zachary@>
2017-12-14 11:58:14 -0800

    add card effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function Album(bandname, title, year, art) {
+  this.inputtedName = bandname;
+  this.inputtedTitle = title;
+  this.inputtedYear = year;
+  this.inputtedURL = art;
+}
+Album.prototype.albumDetails = function() {
+  var thisAlbum = [this.inputtedName, this.inputtedTitle, this.inputtedYear, this.inputtedURL];
+  return thisAlbum;
+}
+var coloringBook = new Album("Chance the Rapper", "Coloring Book", "2016", "http://chanceraps.com/shop/coloring-book-poster")
+
 function resetFields() {
     $("input#band-name").val("");
     $("input#title").val("");
@@ -13,8 +25,10 @@ $(document).ready(function() {
     var inputtedTitle = $("input#title").val();
     var inputtedYear = $("input#year").val();
     var inputtedURL = $("input#art").val();
-    $("#album-info").append(
-      "<div>" + inputtedName + inputtedTitle + inputtedYear + "<img src=" + inputtedURL + ">" + "</div>");
+    // $("#album-info").append(
+    //   "<div>" + inputtedName + inputtedTitle + inputtedYear + "<img src=" + inputtedURL + ">" + "</div>" + coloringBook);
+    var yourAlbum = new Album("<div class='card'>"  + "<img class='show' src=" + inputtedURL + "/>  <ul> <li>" + inputtedName  + "</li> <li>" + inputtedTitle  + "</li> <li>" + inputtedYear  + "</li> </ul> </div>");
+    $("#gallery").append(yourAlbum.albumDetails());
       resetFields();
 
   });
